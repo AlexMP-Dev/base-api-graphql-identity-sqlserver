e771c14ecc1eea6f832cefd494b3d418032feb1a
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-09-01 17:30:34 -0500

    Create Mutation.cs

---
+++ Mutation.cs
@@ -0,0 +1,12 @@
+//mutaciones para graphql
+public class Mutation
+{
+    //public async Task<Producto> AddProducto(Producto producto, [Service] CatalogoContext context)
+    //{
+    //    context.Productos.Add(producto);
+    //    await context.SaveChangesAsync();
+    //    return producto;
+    //}
+
+    // Similar mutations for Cliente and Pedido
+}
